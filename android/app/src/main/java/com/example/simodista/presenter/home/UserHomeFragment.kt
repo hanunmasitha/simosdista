0295857217b388adf9043e19e751f9d1763f9a1d
faiz <faizarfiansyah@gmail.com>
2021-05-29 21:08:50 +0700

    Firebase register and Firestore create user

--- android/app/src/main/java/com/example/simodista/presenter/home/UserHomeFragment.kt
+++ android/app/src/main/java/com/example/simodista/presenter/home/UserHomeFragment.kt
@@ -1,13 +1,18 @@
 package com.example.simodista.presenter.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.simodista.R
+import com.example.simodista.databinding.FragmentUserHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class UserHomeFragment : Fragment() {
+    lateinit var binding: FragmentUserHomeBinding
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +20,14 @@ class UserHomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentUserHomeBinding.bind(view)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        Log.d("Current User", firebaseAuth.currentUser?.email.toString())
     }
 
 }
