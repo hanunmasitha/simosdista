6a4ce4337b562220619b8d32b6546c08e2e13a48
faiz <faizarfiansyah@gmail.com>
2021-05-29 19:11:10 +0700

    Change UI design

--- android/app/src/main/java/com/example/simodista/presenter/register/RegisterFragment.kt
+++ android/app/src/main/java/com/example/simodista/presenter/register/RegisterFragment.kt
@@ -1,4 +1,18 @@
 package com.example.simodista.presenter.register
 
-class RegisterFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.simodista.R
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
 }
