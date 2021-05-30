b3878f364c65cf853875c5633295f3a044620fe2
faiz <faizarfiansyah@gmail.com>
2021-05-30 13:12:19 +0700

    Add Create Report on user

---
+++ android/app/src/main/java/com/example/simodista/presenter/report/CreateReportFragment.kt
@@ -0,0 +1,69 @@
+package com.example.simodista.presenter.report
+
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
+import com.example.simodista.R
+import com.example.simodista.databinding.FragmentCreateReportBinding
+import java.io.File
+import java.util.*
+
+
+class CreateReportFragment : Fragment() {
+    lateinit var binding: FragmentCreateReportBinding
+    private val FILE_NAME = "photo.jpg"
+    private val REQUEST_CODE = 42
+    private lateinit var photoFile: File
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_report, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCreateReportBinding.bind(view)
+        binding.button2.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photoFile = getPhotoFile(FILE_NAME)
+
+            val fileProvider = FileProvider.getUriForFile(requireContext(), "com.example.android.FileProvider", photoFile)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+            if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_CODE)
+            } else {
+                Toast.makeText(requireContext(), "Unable to open camera", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun getPhotoFile(fileName: String): File {
+        val storageDirectory = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+            binding.imageView.setImageBitmap(takenImage)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+    }
+
+
+}
